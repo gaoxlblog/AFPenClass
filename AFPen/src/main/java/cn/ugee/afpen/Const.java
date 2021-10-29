1f429aae94157906b46cfede67400b43d31b308f
gaoxl <shendaocun@icloud.com>
2021-10-30 00:25:44 +0800

    con

---
+++ AFPen/src/main/java/cn/ugee/afpen/Const.java
@@ -0,0 +1,19 @@
+package cn.ugee.afpen;
+
+public class Const
+{
+	public class Broadcast
+	{
+		public static final String PEN_ADDRESS = "pen_address";
+        public static final String PEN_NAME = "pen_NAME";
+		public static final String ACTION_PEN_MESSAGE = "action_pen_message";
+		public static final String MESSAGE_TYPE = "message_type";
+		public static final String CONTENT = "content";
+
+		public static final String ACTION_PEN_DOT = "action_pen_dot";
+		public static final String EXTRA_DOT = "dot";
+
+        public static final String ACTION_FIND_DEVICE = "ACTION_FIND_DEVICE";
+	}
+
+}
